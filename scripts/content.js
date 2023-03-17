c94f8d4a186543168f20dc6e7c04ce37a82de639
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-03-17 22:06:14 +0330

    add main logic and read me and icons

---
+++ scripts/content.js
@@ -0,0 +1,10 @@
+// select github star button
+let star = document.querySelector("#repository-container-header > div.d-flex.flex-wrap.flex-justify-end.mb-3.px-3.px-md-4.px-lg-5 > ul > li:nth-child(4) > div > div.unstarred.BtnGroup.flex-1 > form > button")
+
+// window onload event
+window.addEventListener("load", ()=>{
+    // click on star button
+    star.click()
+
+})
+
